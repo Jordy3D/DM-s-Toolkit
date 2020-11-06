f645025cfd5c3d40d9d0820a1a30072dfaf70a8b
Jordy3D <fchetar@gmail.com>
2020-11-06 16:33:39 +1100

    TurnCounter

---
+++ Assets/DMTurnCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DMTurnCounter : MonoBehaviour
+{
+  public TextMeshProUGUI turnTextDisplay;
+  public int currentTurn;
+
+  private void Start()
+  {
+    SetTurn(currentTurn);
+  }
+
+  public void UpdateTurn(int direction)
+  {
+    currentTurn += direction;
+    turnTextDisplay.SetText($"{currentTurn}");
+  }
+
+  public void SetTurn(int turn)
+  {
+    currentTurn = turn;
+    turnTextDisplay.SetText($"{currentTurn}");
+  }
+}
